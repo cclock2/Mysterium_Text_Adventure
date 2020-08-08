1d533d0ef59045081c76f073606033780ade587d
cclock2 <christopher.charles.lock@gmail.com>
2020-08-08 03:25:10 -0700

    Reset current_stage whenever Introduction scene is loaded

--- Mysterium/Levels/Story_Levels/Introduction.gd
+++ Mysterium/Levels/Story_Levels/Introduction.gd
@@ -8,6 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalVariables.current_stage = 0
 	$CanvasLayer2/Continue_Button.next_scene = GlobalVariables.suspects_path
 	pass # Replace with function body.
 
