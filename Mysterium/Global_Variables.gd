5b1da4da6974f8fc5f499aafa05223fd9728e67f
cclock2 <christopher.charles.lock@gmail.com>
2020-08-08 04:04:30 -0700

    Connect Scenes: Rooms -> Coorect_Room -> Weapons

--- Mysterium/Global_Variables.gd
+++ Mysterium/Global_Variables.gd
@@ -14,10 +14,11 @@ var level_gameplay_path = "/root/Level_Gameplay"
 #scene path
 var suspects_path = 	"res://Levels/Gameplay_Levels/Suspects.tscn"
 var rooms_path = 		"res://Levels/Gameplay_Levels/Rooms.tscn"
+var weapons_path = 		"res://Levels/Gameplay_Levels/Weapons.tscn"
 var introduction_path = "res://Levels/Story_Levels/Introduction.tscn"
 var game_over_path = 	"res://Levels/Story_Levels/Game_Over.tscn"
 
-var success_path_array = ["res://Levels/Story_Levels/Correct_Suspect.tscn","",""]
+var success_path_array = ["res://Levels/Story_Levels/Correct_Suspect.tscn","res://Levels/Story_Levels/Correct_Room.tscn",""]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
